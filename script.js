dfe503bf74835c4c499afb4dad02994603ef56d1
Saidcode2024 <appcode2024@gmail.com>
2024-07-22 23:58:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var server_urlbook = {}
 
 
 server_urlbook[0] = [
-    ['15-BTaQ8pAYqMGYVq3cNtfDf1GrLflBws' , 'أغنى رجل فى بابل' , 'AR' , '120' , 'about book 12' , 'https://drive.google.com/file/d/15HpW5wAOpMsSxYx7ZI-RNMNeC_J9AUfB/view?usp=drivesdk']
+    ['15-BTaQ8pAYqMGYVq3cNtfDf1GrLflBws' , 'أغنى رجل فى بابل' , 'AR' , '120' , 'about book' , 'https://drive.google.com/file/d/15HpW5wAOpMsSxYx7ZI-RNMNeC_J9AUfB/view?usp=drivesdk']
 ] 
 
 server_urlbook[1] = [
