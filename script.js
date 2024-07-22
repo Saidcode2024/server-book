1d4e6c700d02e4c427ebdb1bfcc0e9af28f7488a
Saidcode2024 <appcode2024@gmail.com>
2024-07-22 21:35:15 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var server_filter = ['كتب تهمك','كتب تنمية']
+
+var server_urlbook = {}
+
+
+
+server_urlbook[0] = [
+    ['15-BTaQ8pAYqMGYVq3cNtfDf1GrLflBws' , 'أغنى رجل فى بابل' , 'AR' , '120' , 'about book' , 'https://drive.google.com/file/d/15HpW5wAOpMsSxYx7ZI-RNMNeC_J9AUfB/view?usp=drivesdk']
+] 
+
+server_urlbook[1] = [
+
+] 
