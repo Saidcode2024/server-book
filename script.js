094f35bd7e47199dbe6ed29e0a2ab18c4e700c4c
Saidcode2024 <appcode2024@gmail.com>
2024-07-29 00:33:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-var server_filter = ['كتب تهمك','كتب تنمية']
+var server_filter = [
+    /*n1*/ 'التنمية البشرية وتطوير الذات',
 
-var server_urlbook = {}
+]
+var server_books = {}
+var img_load = "https://drive.google.com/thumbnail?id="
+server_books[1000] = [
+    /*n1*/ [
+        /*img*/  img_load + '1C-D2dApVTTfBrY3D2tQf7Esq7xAlsT67' ,
+        /*name*/ 'كتاب كيف تؤثر على الآخرين وتكتسب الأصدقاء',
+        /*have*/ 'ديل كارنيجي',
+        /*book*/ 'https://drive.google.com/file/d/1COP3I5uZgNIYb5foTYhi9iYpO-9Q4nWR/view?usp=drivesdk',
+        /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
+    ] ,
+]
 
+server_books[0] = [
+    /*n1*/ [
+        /*img*/  img_load + '1C-D2dApVTTfBrY3D2tQf7Esq7xAlsT67' ,
+        /*name*/ 'كتاب كيف تؤثر على الآخرين وتكتسب الأصدقاء',
+        /*have*/ 'ديل كارنيجي',
+        /*book*/ 'https://drive.google.com/file/d/1COP3I5uZgNIYb5foTYhi9iYpO-9Q4nWR/view?usp=drivesdk',
+        /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
+    ] ,
+]
 
+server_books[1] = [
 
-server_urlbook[0] = [
-    ['15-BTaQ8pAYqMGYVq3cNtfDf1GrLflBws' , 'أغنى رجل فى بابل' , 'AR' , '220' , 'about book' , 'https://drive.google.com/file/d/15HpW5wAOpMsSxYx7ZI-RNMNeC_J9AUfB/view?usp=drivesdk']
-] 
+]
 
-server_urlbook[1] = [
-
-] 
