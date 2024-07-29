ed9eb8fe8e61230d13251a59b9b106b3027f0e8a
Saidcode2024 <appcode2024@gmail.com>
2024-07-29 17:35:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var server_filter = [
     /*n1*/ 'التنمية البشرية وتطوير الذات',
     /*n2*/ 'علم النفس',
+    /*n3*/ 'الشعر',
+    /*n4*/ 'التاريخ والحضارات',
+    /*n5*/ 'العلوم الاسلامية',
 ]
 var server_books = {}
 var img_load = "https://drive.google.com/thumbnail?id="
@@ -13,7 +16,7 @@ server_books[1000] = [
         /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
     ] ,
 ]
-
+// n1
 server_books[0] = [
     /*n1*/ [
         /*img*/  img_load + '1C-D2dApVTTfBrY3D2tQf7Esq7xAlsT67' ,
@@ -23,8 +26,25 @@ server_books[0] = [
         /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
     ] ,
 ]
-
+// n2
 server_books[1] = [
 
 ]
+// n3
+server_books[2] = [
+
+]
+// n4
+server_books[3] = [
+
+]
+// n5
+server_books[4] = [
+
+]
+
+
+
+
+
 
