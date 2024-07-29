3ae8cbb59f172ba11a32e2586860e0ac28109f75
Saidcode2024 <appcode2024@gmail.com>
2024-07-29 18:08:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ server_books[1000] = [
         /*book*/ 'https://drive.google.com/file/d/1COP3I5uZgNIYb5foTYhi9iYpO-9Q4nWR/view?usp=drivesdk',
         /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
     ] ,
+    /*n2*/ [
+        /*img*/  img_load + '1CwNkDKVmpK1SQUSODbAPLke8ASUE0Gnu' ,
+        /*name*/ 'كتاب شجاعة أن تكون غير محبوب',
+        /*have*/ 'مجموعة مؤلفين',
+        /*book*/ 'https://drive.google.com/file/d/1CwtzIs8hNE3WmgIvo2_geuR9XVfLe1of/view?usp=drivesdk',
+        /*page*/ 'html/كتاب_شجاعة_أن_تكون_غير_محبوب.html'
+    ] ,
 ]
 // n1
 server_books[0] = [
@@ -24,6 +31,13 @@ server_books[0] = [
         /*have*/ 'ديل كارنيجي',
         /*book*/ 'https://drive.google.com/file/d/1COP3I5uZgNIYb5foTYhi9iYpO-9Q4nWR/view?usp=drivesdk',
         /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
+    ] ,
+    /*n2*/ [
+        /*img*/  img_load + '1CwNkDKVmpK1SQUSODbAPLke8ASUE0Gnu' ,
+        /*name*/ 'كتاب شجاعة أن تكون غير محبوب',
+        /*have*/ 'مجموعة مؤلفين',
+        /*book*/ 'https://drive.google.com/file/d/1CwtzIs8hNE3WmgIvo2_geuR9XVfLe1of/view?usp=drivesdk',
+        /*page*/ 'html/كتاب_شجاعة_أن_تكون_غير_محبوب.html'
     ] ,
 ]
 // n2
@@ -42,9 +56,3 @@ server_books[3] = [
 server_books[4] = [
 
 ]
-
-
-
-
-
-
