51c7138b625d112a958ca93b5981f08b1ab4f921
Saidcode2024 <appcode2024@gmail.com>
2024-08-07 13:34:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,31 +14,31 @@ server_books[0] = [
         /*img*/  img_load + '1C-D2dApVTTfBrY3D2tQf7Esq7xAlsT67&sz=w500-h735' ,
         /*name*/ 'كتاب كيف تؤثر على الآخرين وتكتسب الأصدقاء',
         /*have*/ 'ديل كارنيجي',
-        /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
+        /*page*/ 'books/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
     ] ,
     /*n2*/ [
         /*img*/  img_load + '1DxpifjKUCOKwrML2sFRg75JNSYdKyAbK&sz=w500-h735' ,
         /*name*/ 'كتاب شجاعة أن تكون غير محبوب',
         /*have*/ 'مجموعة مؤلفين',
-        /*page*/ 'html/كتاب_شجاعة_أن_تكون_غير_محبوب.html'
+        /*page*/ 'books/كتاب_شجاعة_أن_تكون_غير_محبوب.html'
     ] ,
     /*n3*/ [
         /*img*/  img_load + '1G2rvPzXlfRj6CHKK_sSgezpsP3PuWMpW&sz=w452-h678' ,
         /*name*/ 'كتاب خذها بقوة',
         /*have*/ 'حسام عبد العزيز',
-        /*page*/ 'html/كتاب_خذها_بقوة.html'
+        /*page*/ 'books/كتاب_خذها_بقوة.html'
     ] ,
     /*n4*/ [
         /*img*/  img_load + '1IMLxSPpx17Sr7dxLmG4Xko9bGiXgxhd0&sz=w307-h455' ,
         /*name*/ 'كتاب علم النفس الأسود',
         /*have*/ 'د. محمد عبد العال',
-        /*page*/ 'html/كتاب_علم_النفس_الأسود.html'
+        /*page*/ 'books/كتاب_علم_النفس_الأسود.html'
     ] ,
     /*n5*/ [
         /*img*/  img_load + '1JF8PYiYYAWMzzbOoTWbamcxK_GUGXQYX&sz=w679-h1000' ,
         /*name*/ 'رواية أحدب نوتردام',
         /*have*/ 'فيكتور هيجو',
-        /*page*/ 'html/رواية_أحدب_نوتردام.html'
+        /*page*/ 'books/رواية_أحدب_نوتردام.html'
     ] ,
 ]
 // n1
@@ -47,19 +47,19 @@ server_books[1] = [
         /*img*/  img_load + '1C-D2dApVTTfBrY3D2tQf7Esq7xAlsT67&sz=w500-h735' ,
         /*name*/ 'كتاب كيف تؤثر على الآخرين وتكتسب الأصدقاء',
         /*have*/ 'ديل كارنيجي',
-        /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
+        /*page*/ 'books/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
     ] ,
     /*n2*/ [
         /*img*/  img_load + '1DxpifjKUCOKwrML2sFRg75JNSYdKyAbK&sz=w500-h735' ,
         /*name*/ 'كتاب شجاعة أن تكون غير محبوب',
         /*have*/ 'مجموعة مؤلفين',
-        /*page*/ 'html/كتاب_شجاعة_أن_تكون_غير_محبوب.html'
+        /*page*/ 'books/كتاب_شجاعة_أن_تكون_غير_محبوب.html'
     ] ,
     /*n3*/ [
         /*img*/  img_load + '1G2rvPzXlfRj6CHKK_sSgezpsP3PuWMpW&sz=w452-h678' ,
         /*name*/ 'كتاب خذها بقوة',
         /*have*/ 'حسام عبد العزيز',
-        /*page*/ 'html/كتاب_خذها_بقوة.html'
+        /*page*/ 'books/كتاب_خذها_بقوة.html'
     ] ,
 ]
 // n2
@@ -68,7 +68,7 @@ server_books[2] = [
         /*img*/  img_load + '1IMLxSPpx17Sr7dxLmG4Xko9bGiXgxhd0&sz=w307-h455' ,
         /*name*/ 'كتاب علم النفس الأسود',
         /*have*/ 'د. محمد عبد العال',
-        /*page*/ 'html/كتاب_علم_النفس_الأسود.html'
+        /*page*/ 'books/كتاب_علم_النفس_الأسود.html'
     ] ,
 ]
 // n3
@@ -77,7 +77,7 @@ server_books[3] = [
         /*img*/  img_load + '1JF8PYiYYAWMzzbOoTWbamcxK_GUGXQYX&sz=w679-h1000' ,
         /*name*/ 'رواية أحدب نوتردام',
         /*have*/ 'فيكتور هيجو',
-        /*page*/ 'html/رواية_أحدب_نوتردام.html'
+        /*page*/ 'books/رواية_أحدب_نوتردام.html'
     ] ,
 ]
 // n4
